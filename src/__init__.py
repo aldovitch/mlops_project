1193631562ba8472145a46390f5d3c9ee0b27555
Aldo <aldo.barduco@gmail.com>
2026-05-31 14:18:59 -0300

    MLflow part 3

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,9 @@
 import logging
+from dotenv import load_dotenv
 
+# Load environment variables from .env file
+load_dotenv()
+  
 # Configure the logging strategy
 logging.basicConfig(
     level=logging.INFO, # DEBUG, INFO, WARNING, ERROR, CRITICAL
