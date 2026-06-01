18107b7b635b2ed5480791051c16713d48a9b007
Aldo <aldo.barduco@gmail.com>
2026-06-01 12:10:46 -0300

    pyproject and data_load changes

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,16 @@
 import logging
 from dotenv import load_dotenv
 
+import dagshub
+
 # Load environment variables from .env file
 load_dotenv()
-  
+
+dagshub.init(  # Initialize DagsHub integration
+    repo_owner='aldovitch',
+    repo_name='mlops_project'    
+)
+
 # Configure the logging strategy
 logging.basicConfig(
     level=logging.INFO, # DEBUG, INFO, WARNING, ERROR, CRITICAL
